65c84bf3f0dc52ae254589af9b00ccadb785404b
Ratmir Ishmuratov <nepstif@gmail.com>
2023-09-26 22:37:18 +0500

    new format

--- src/NamePrint.java
+++ src/NamePrint.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class NamePrint {
     public static void main(String[] args) {
 
-        String name = "Вячеслав";
+        String name = "Р’СЏС‡РµСЃР»Р°РІ";
 
         System.out.print("Please enter the required name: ");
         try (Scanner scanner = new Scanner(System.in)) {
             String getName = scanner.nextLine();
             scanner.close();
             if (getName.equalsIgnoreCase(name)) {
-                System.out.println("Привет, " + name);
+                System.out.println("РџСЂРёРІРµС‚ , " + name);
             } else {
-                System.out.println("Нет такого имени");
+                System.out.println("РќРµС‚ С‚Р°РєРѕРіРѕ РёРјРµРЅРё");
             }
         }
     }
